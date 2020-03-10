450babed070db5af3974ff808221734a2b8482a1
jpatrickcm <jeanpatrick.94@gmail.com>
2020-03-10 07:14:25 -0500

    avance pichangas

---
+++ tdd-pichangas.js
@@ -0,0 +1,22 @@
+let pichanga = {
+    fecha: "11-03-2020",
+    hora: "20:00",
+    lugar: "Canchita De Fulbito Simon Bolivar",
+    direccion: "asdas",
+    ubicacionMaps: "https://maps.app.goo.gl/iZEg6MZWnLLTSJU36"
+};
+
+let participante = {
+    nombre: 'Jean Patrick',
+    alias: 'Loco',
+    nroContacto: '+51999947642'
+}
+
+let participantes = [
+
+];
+
+participantes.push(participante);
+
+console.log(pichanga);
+console.log(participantes);
