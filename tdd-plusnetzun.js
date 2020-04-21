7b3e803821239d5aee9c23019518298f49a7f1c3
jcampos <jcampos@digiflow.pe>
2020-04-21 11:15:10 -0500

    caso cursos plus netzun , template en vue.js

---
+++ tdd-plusnetzun.js
@@ -0,0 +1,6 @@
+
+let user = 'lizmale@hotmail.com';
+
+let classdivCourses = document.getElementsByClassName('item-my-course');
+classdivCourses[0].childNodes[0];
+classdivCourses[0].childNodes[0].childNodes[0].textContent;
